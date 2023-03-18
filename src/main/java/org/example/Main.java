43873185158752f13a9eec1107fc02f787d178a3
Nguyen Viet Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2023-03-18 20:48:44 +0700

    Complte CyclicRotation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
 import org.example.codility.BinaryGap;
+import org.example.codility.CyclicRotation;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryGap binaryGap = new BinaryGap();
-        System.out.println(binaryGap.csolution(20));
+        int[] A =  {1, 2, 3, 4};
+        int K = 4;
+        CyclicRotation cyclicRotation = new CyclicRotation();
+        for (int i = 0; i < A.length; i++) {
+            System.out.println(cyclicRotation.solution(A,K)[i]);
+        }
     }
 
 }
---
+++ src/main/java/org/example/codility/CyclicRotation.java
@@ -0,0 +1,18 @@
+package org.example.codility;
+
+public class CyclicRotation {
+    public int[] solution(int[] A, int K) {
+        int[] rsArray = new int[A.length];
+        if(A.length==0)
+            return rsArray;
+        if(K>A.length){
+            K=K%A.length;
+        }
+        for (int i = 0; i < rsArray.length; i++) {
+            if(i+K>rsArray.length-1){
+                rsArray[i+K-A.length] = A[i];
+            }else rsArray[i+K] = A[i];
+        }
+        return rsArray;
+    }
+}
